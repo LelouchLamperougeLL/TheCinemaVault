a88aa48fec28f9a3b6d4537f81412e90339efa56
Sabique Beg <92996594+LelouchLamperougeLL@users.noreply.github.com>
2026-05-18 04:11:59 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,6 +14,8 @@ const STATIC_ASSETS = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png',
   './favicon.png',
   './site_logo.png'
 ];
